e857ed0d3fee47f88c3e15883486b4183ce99dd3
FonFonexe <143347189+FonFonexe@users.noreply.github.com>
2023-12-13 01:08:13 +0700

    Update sender.py

--- sender.py
+++ sender.py
@@ -4,22 +4,19 @@ import requests
 import data
 
 
-def new_order():
-    return requests.post(configuration.BASE_URL + configuration.CREATE_ORDER, json=data.CREATE_ORDER_BODY)
+def test_create_order():
+    def new_order():
+        return requests.post(configuration.BASE_URL + configuration.CREATE_ORDER, json=data.CREATE_ORDER_BODY)
 
-
-def aut_track():
     order = new_order()
-    return order.json()["track"]
+    track = order.json()["track"]
+
+    url = configuration.BASE_URL + configuration.GET_ORDER + str(track)
+    response = requests.get(url)
+
+    assert response.status_code == 200
+    return response.json()
 
 
-def get_order():
-    url = configuration.BASE_URL + configuration.GET_ORDER + str(aut_track())
-    print(url)
-    return requests.get(url)
-
-
-respons = get_order()
-assert respons.status_code == 200
-print(respons.json())
-print(respons.status_code)
+result = test_create_order()
+print(result)
